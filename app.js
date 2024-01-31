4644b03fca7778364afb2aa5bdd8dc63aa90d9a6
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-01-31 18:10:44 +0100

    function types and callback

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-//* UNION TYPE
-// come essere piu flessibile sui type
-//per accettare 2 o piu tipi di valori usiamo |
-function combine(input1, input2, resultConvertion //literal type combinato con un union type
-) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConvertion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    //   if (resultConvertion === "as-number") {
-    //     return +result;
-    //   } else {
-    //     return result.toString();
-    //   }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(20, 30, "as-number");
-console.log(combinedAges);
-var combinedName = combine("Thomas", "Elena", "as-text");
-console.log(combinedName);
-//Literl Types
-var CombinedStringAges = combine("30", "26", "as-number");
-console.log(CombinedStringAges);
+function printResult(num) {
+    console.log("Result: " + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(4, 12));
+//function type che accetta 2 numeri come parametri e rilascia un numero come risultato
+var combinedValues;
+combinedValues = add;
+console.log(combinedValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
